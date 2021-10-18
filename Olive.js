6a78b455b4fd444bfd7e7aad22cedf8d500c3f82
itselectroz <harry.whittle@cwgsy.net>
2021-10-18 15:55:46 +0100

    Mostly done. I apologise for how god awful this code is

--- Olive.js
+++ Olive.js
@@ -1,39 +1,99 @@
+function makeNetwork(nodes, cons) {
+    let network = new Network();
+
+    for (let i = 0; i < cons.length; i++) {
+        let conn = cons[i];
+        conn.in = nodes[conn.in];
+        conn.out = nodes[conn.out];
+        network.addConnection(conn);
+    }
+
+    return network;
+}
+
 class Olive {
-    constructor(pos) {
+    constructor(pos, brain) {
         this.pos = !!pos ? pos : new Vector2(0, 0);
         this.velocity = new Vector2(0, 0);
         this.acceleration = new Vector2(0, 0);
-        
+
         this.image = new Image();
         this.image.src = 'data/olive.png';
 
         this.crashed = false;
         this.score = 0;
+
+        if(!brain ) {
+            this.brain = makeNetwork([
+                new Node(1, 0), // pipe height
+                new Node(2, 3) // output
+            ], [
+                new ConnectionGene(0, 1, Math.random(), true, 1),
+            ]);
+    
+            // Randomise brain
+            const nodeNum = Math.floor(Math.random() * 5);
+            for (let i = 0; i < nodeNum; i++) {
+                this.brain.addRandomNode();
+            }
+    
+            const connectionNum = Math.floor(Math.random() * 5);
+            for (let i = 0; i < connectionNum; i++) {
+                this.brain.addRandomConnection();
+            }
+    
+            // const mutationNum = Math.floor(Math.random() * 5);
+            // for (let i = 0; i < mutationNum; i++) {
+            //     this.brain.mutate(true);
+            // }
+    
+            this.brain.buildNetwork();
+        }
+        else {
+            this.brain = brain;
+        }        
     }
 
     applyForce(force) {
-        if(!force || !(force instanceof Vector2)) { 
+        if (!force || !(force instanceof Vector2)) {
             return false;
         }
         this.acceleration = this.acceleration.add(force);
         return true;
     }
 
-    update(frame) {
+    update(frame, closestPipeY) {
+        if (this.crashed)
+            return;
+
+        const dist = (this.pos.y - closestPipeY)
+
+        if (frame % 10 == 0) {
+
+            const output = this.brain.feedForward([
+                dist
+            ]);
+
+            if (output >= 0.75) {
+                this.velocity = new Vector2(0, 0);
+                this.applyForce(new Vector2(0, -400));
+            }
+        }
+
         this.velocity = this.velocity.add(this.acceleration).limit(maxVelocity);
         this.pos = this.pos.add(this.velocity.div(100));
         this.acceleration = new Vector2(0, 0);
 
-        if(this.pos.y < 0) {
+        if (this.pos.y < 0) {
             this.pos.y = 0;
         }
-        if(this.pos.y > window.innerHeight) {
+        if (this.pos.y > window.innerHeight) {
             this.pos.y = window.innerHeight;
         }
     }
 
     draw(ctx) {
-        if(this.crashed)
+        if (this.crashed)
             return;
         ctx.drawImage(this.image, this.pos.x - 25, this.pos.y - 25, 50, 50);
     }
--- index.js
+++ index.js
@@ -1,11 +1,23 @@
+// Lets think about network design
+
+/* 
+Inputs:
+    - Height to center of next pipe
+    - current velocity
+
+Outputs:
+    - jump
+
+*/
+
+
 let canvas, ctx;
+let selectedOlive, prevBestOlive;
 
 let frame = 0;
 
 const olives = [];
 const pipes = [];
-
-
 
 function resizeCanvas() {
     canvas.width = window.innerWidth;
@@ -16,12 +28,12 @@ function resizeCanvas() {
 
 function events() {
     window.addEventListener('resize', resizeCanvas);
-    window.addEventListener('keypress', () => {
-        for(const olive of olives) {
-            olive.velocity = new Vector2(0, 0);
-            olive.applyForce(new Vector2(0, -400));
-        }
-    })
+    // window.addEventListener('keypress', () => {
+    //     for(const olive of olives) {
+    //         olive.velocity = new Vector2(0, 0);
+    //         olive.applyForce(new Vector2(0, -400));
+    //     }
+    // })
 }
 
 function setupPipes() {
@@ -41,16 +53,62 @@ function setupOlives() {
 function nextGeneration() {
     // perform selection, crossover, mutation
 
-    setupOlives();
+    let highestScore = 0;
+    let bestOlive = null;
+    for(const olive of olives) {
+        if(olive.score > highestScore) {
+            highestScore = olive.score;
+            bestOlive = olive;
+        }
+    }
+
+    prevBestOlive = bestOlive;
+
+    // selection
+    const selectionPool = [];
+    for(const olive of olives) {
+        const count = olive.score * 100 + 1;
+        for(let i = 0; i < count; i++) {
+            selectionPool.push(olive.brain);
+        }
+    }
+
+    for(let i = 0; i < populationSize; i++) {
+        const father = selectionPool[Math.floor(Math.random() * selectionPool.length)];
+        const mother = selectionPool[Math.floor(Math.random() * selectionPool.length)];
+
+        // Crossover
+        const newBrain = Network.cross(father, mother);
+
+        // mutation
+        newBrain.mutate();
+
+        newBrain.buildNetwork();
+
+        const variation = (Math.random() * 200) - 100
+        olives[i] = new Olive(new Vector2(window.innerWidth / 3, (window.innerHeight / 2) + variation), newBrain);
+    }
 
     setupPipes();
 
+    selectedOlive = null;
 }
 
 function update() {
+    const oliveX = window.innerWidth / 3;
+    let closestX = Infinity
+    let closestY = Infinity
+    for(const pipe of pipes) {
+        const dist = pipe.pos.x - oliveX;
+        if(dist > 0 && dist < closestX) {
+            closestX = dist;
+            closestY = pipe.pos.y;
+        }
+    }
+
     let alive = false;
     for(const olive of olives) {
-        olive.update(frame);
+        olive.update(frame, closestY);
         if(!olive.crashed) {
             alive = true;
         }
@@ -58,6 +116,7 @@ function update() {
 
     if(!alive) {
         nextGeneration();
+        return;
     }
 
     for(let i = 0; i < pipes.length; i++) {
@@ -90,6 +149,9 @@ function update() {
             if(pipe.pos.x < window.innerWidth / 3 && !pipe.scored) {
                 for(const olive of olives) {
                     olive.score++;
+                    if(olive.score >= 20) {
+                        nextGeneration();
+                    }
                 }
                 pipe.scored = true;
             }
@@ -100,6 +162,116 @@ function update() {
                 }
             }
         }
+    }
+}
+
+function getCurrentHighestFitness() {
+    let highestScore = 0;
+    for(const olive of olives) {
+        if(olive.score > highestScore) {
+            highestScore = olive.score;
+        }
+    }
+    return highestScore;
+}
+
+function drawNetwork(ctx, network) {
+    const locations = {};
+
+    let drawX = window.innerWidth - 500 + 25;
+
+    let maxLevel = 0;
+
+    // Draw all but output nodes
+    const nodesToDraw = [];
+    for(const node of network.nodes) {
+        if(node.type == 3) {
+            continue;
+        }
+
+        // If input node
+        if(node.type <= 1) {
+            if(!nodesToDraw[0]) {
+                nodesToDraw[0] = [];
+            }
+
+            nodesToDraw[0].push(node);
+        }
+        else {
+            const level = network.connections.filter(conn => conn.out.id == node.id).length;
+            if(!nodesToDraw[level]) {
+                nodesToDraw[level] = [];
+            }
+
+            nodesToDraw[level].push(node);
+
+            if(level > maxLevel) {
+                maxLevel = level;
+            }
+        }
+    }
+
+
+    for(let level = 0; level < nodesToDraw.length; level++) {
+        const nodes = nodesToDraw[level];
+        if(!nodes) {
+            continue;
+        }
+        for(let i = 0; i < nodes.length; i++) {
+            const node = nodes[i];
+            if(node.type == 0 || node.type == 1) {
+                ctx.fillStyle = "green";
+            }
+            else if(node.type == 2) {
+                ctx.fillStyle = "white";
+            }
+    
+            ctx.beginPath();
+            ctx.arc(drawX + 50 * level + 20 * i, 150 + 45 * i, 15, 0, 2 * Math.PI);
+            ctx.fill();
+
+            locations[node.id] = {
+                x: drawX + 50 * level + 20*i,
+                y: 150 + 45 * i
+            };
+        }
+    }
+
+    // draw output nodes
+    let count = 2;
+    ctx.fillStyle = "red";
+    for(let i = 0; i < network.nodes.length; i++) {
+        const node = network.nodes[i];
+        if(node.type == 3) {
+            ctx.beginPath();
+            ctx.arc(drawX + (50 * (maxLevel + 1)) + 20*count, 150 + 45 * count, 15, 0, 2 * Math.PI);
+            ctx.fill();
+
+            locations[node.id] = {
+                x: drawX + (50 * (maxLevel + 1)) + 20*count,
+                y: 150 + 45 * count
+            };
+
+            count++;
+        }
+    }
+
+    // Draw connections
+
+    for(const conn of network.connections) {
+        const colourStyle = conn.enabled ? "white" : "gray";
+
+        ctx.beginPath();
+        ctx.lineWidth = 5;
+        ctx.fillStyle = colourStyle;
+        ctx.strokeStyle = colourStyle;
+
+        const pointA = locations[conn.in.id];
+        const pointB = locations[conn.out.id];
+
+        ctx.moveTo(pointA.x, pointA.y);
+        ctx.lineTo(pointB.x, pointB.y);
+        ctx.stroke();
     }
 }
 
@@ -119,6 +291,24 @@ function draw() {
 
         olive.draw(ctx);
     }
+
+    // Black square in top right
+    ctx.fillStyle = "black";
+    ctx.fillRect(rect.width - 500, 0, 500, 300);
+
+    if(!selectedOlive || selectedOlive.crashed) {
+        const selectionPool = olives.filter(v => !v.crashed);
+        selectedOlive = selectionPool[Math.floor(Math.random() * selectionPool.length)];
+    }
+
+    ctx.fillStyle = "white";
+    ctx.font = "30px Arial";
+    ctx.fillText(`Previous Best Score: ${!!prevBestOlive ? prevBestOlive.score : '0'}`, rect.width - 500, 40);
+
+    const highestFitness = getCurrentHighestFitness();
+    ctx.fillText(`Current highest score: ${highestFitness}`, rect.width - 500, 75);
+
+    drawNetwork(ctx, selectedOlive.brain);
 }
 
 function setup() {
